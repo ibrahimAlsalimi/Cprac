fae0f6286e4083959209ccf853b0c61363474c4c
ibrahimAlsalimi <ibrahim_alharbe@pm.me>
2026-09-03 13:47:03 +0300

    some edits and planing

--- ssysr/ssysr.c
+++ ssysr/ssysr.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#define WORD8 8
+
 int Str_to_int(char ch, int count){
   int ret_val = ch - 48;
   
@@ -17,17 +19,19 @@ int Str_to_int(char ch, int count){
 
 int main(){
 
+  float KbToGib = 1024 * 1024;
+
+
+      while (true) {
   FILE *fpCpu = fopen("/proc/meminfo", "r");
   if(fpCpu == NULL){
-    perror("can't open /proc/stat");
+    perror("can't open /proc/meminfo");
     return 1;  
   }
 
-
-  float KbToGib = 1000000;
-
   char ln_buff[32];
-  char total_Mem[16];
+  
+  char total_Mem[WORD8];
   float tMem;
   
 
@@ -41,31 +45,38 @@ int main(){
     int spc = -1;
     int start_point = 1;
 
-
     for(int c = 0; c < sizeof(ln_buff); c++){
       if(ln_buff[c] == ':') col = c;
       if(ln_buff[c] == ' ') spc++;
 
 
     }
+    
+
     start_point += col + spc;
     for(int k = 0; k < 8; k++){
       if(i == 0) total_Mem[k]  = ln_buff[start_point + k];
       if(i == 2) mem_Avi[k]    = ln_buff[start_point + k];
     }
   }
-    for(int i = 7; i >= 0; i--){
+  
+
+  fclose(fpCpu);
+  
+  for(int i = 7; i >= 0; i--){
       tMem += Str_to_int(total_Mem[7-i], i); 
       aviMem += Str_to_int(mem_Avi[7-i], i); 
 
     }
     
 
-      printf("total Mem = %.2f Gib\t", tMem / KbToGib); 
-      printf("Available Mem = %.2f Gib\t", aviMem / KbToGib);
-  
-      printf("used Mem = %.2f Gib\t", (tMem - aviMem)/KbToGib);
-      printf("[%%%.2f]\n", ((tMem - aviMem)/tMem)*100);
-  fclose(fpCpu);
+      //printf("\rtotal Mem = %.2f Gib\t", tMem / KbToGib); 
+      //printf("\rAvailable Mem = %.2f Gib\t", aviMem / KbToGib);
+      //printf("\rused Mem = %.2f Gib\t", (tMem - aviMem)/KbToGib);
+      printf("\r[%%%.2f]\n", ((tMem - aviMem)/tMem)*100);
+      fflush(stdout);
+      usleep(500000);
+    
 
+      }
 }

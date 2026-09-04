4170024a489ed6ef5d177f1aaf7f9cb002a4d50e
ibrahimAlsalimi <ibrahim_alharbe@pm.me>
2026-09-04 22:01:56 +0300

    rewrting the code to make it more better

--- ssysr/ssysr.c
+++ ssysr/ssysr.c
@@ -1,105 +1,35 @@
 /*
- *    |        tasks        |     state     |
- *    |---------------------|---------------|
- *    | need to make a live |       N       |
- *    | version             |               |
- *    |---------------------|---------------|
- *    | add args            |       N       |
- *    |---------------------|---------------|
- *    | clean code          |       N       |
- *    |---------------------|---------------|
+ *  - rewrite the code agine 
+ *  - i will isolte the code blocks and make a function fo every thing 
+ *  - it wolud have args and it eill do multiple things like show the live use fo resoueres
+ *  - -try make it clean ans safe as much as i can 
  *
  *  
  *
- *
- *===================================================
- *  Motes :
- *   now i shoould make it more cleanrt and effcint 
- *   i think it need more more work 
- *
- *
- *
  * */
-
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#define WORD 8
+#define ASCCI_ZERO_DEC 48 
 
 
-#define WORD8 8
+void fetch_Mem_Info(){
 
-int Str_to_int(char ch, int count){
-  int ret_val = ch - 48;
-  
-  if(ch >= 48){
-  for(int i = count; i >= 1; i--) ret_val *= 10;
-    }
-  
-  else ret_val = 0; 
+}
 
-  return ret_val;
+void calc_Cpu_Use(){  
+
+}
+
+void print_ref(){     // refrech print
+
 }
 
 
-
-int main(){
-
-  float KbToGib = 1024 * 1024;
-
-
-  FILE *fpCpu = fopen("/proc/meminfo", "r");
-  if(fpCpu == NULL){
-    perror("can't open /proc/meminfo");
-    return 1;  
-  }
-
-  char ln_buff[32];
-  
-  char total_Mem[WORD8];
-  float tMem;
-  
-
-  char mem_Avi[16];
-  float aviMem;
-
-
-  for(int i = 0; i < 3; i++){
-    fgets(ln_buff, sizeof(ln_buff), fpCpu);
-    int col = 0;
-    int spc = -1;
-    int start_point = 1;
-
-    for(int c = 0; c < sizeof(ln_buff); c++){
-      if(ln_buff[c] == ':') col = c;
-      if(ln_buff[c] == ' ') spc++;
-
-
-    }
-    
-
-    start_point += col + spc;
-    for(int k = 0; k < 8; k++){
-      if(i == 0) total_Mem[k]  = ln_buff[start_point + k];
-      if(i == 2) mem_Avi[k]    = ln_buff[start_point + k];
-    }
-  }
-  
-
-  fclose(fpCpu);
-  
-  for(int i = 7; i >= 0; i--){
-      tMem += Str_to_int(total_Mem[7-i], i); 
-      aviMem += Str_to_int(mem_Avi[7-i], i); 
-
-    }
-    
-
-      //printf("\rtotal Mem = %.2f Gib\t", tMem / KbToGib); 
-      //printf("\rAvailable Mem = %.2f Gib\t", aviMem / KbToGib);
-      //printf("\rused Mem = %.2f Gib\t", (tMem - aviMem)/KbToGib);
-      printf("\r[%%%.2f]\n", ((tMem - aviMem)/tMem)*100);
-      fflush(stdout);
-      usleep(500000);
-    
-
+int main(int argc, char *argv[]){
+ 
+    return 0;
 }
